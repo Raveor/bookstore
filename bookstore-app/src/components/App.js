0101a099969af95842e2841f78dd1cea0f1a5690
Sebastian Zieja <sebastian.zieja@gmail.com>
2019-07-03 00:36:36 +0200

    Better handling logged user # now when user goes to other page by hand they will not get log out

--- bookstore-app/src/components/App.js
+++ bookstore-app/src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import "./App.css";
 import BookTypeList from "./category/BookTypeList";
@@ -17,32 +17,57 @@ import BookTypeForm from "./forms/BookTypeForm";
 import AuthorForm from "./forms/AuthorForm";
 import PublishingHouseForm from "./forms/PublishingHouseForm";
 import BookForm from "./forms/BookForm";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "../utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "../actions/authActions";
 
-function App() {
-    return (
-        <Provider store={store}>
-            <Router>
-                <div className="App">
-                    <Navbar/>
-                    <Route exact path="/" component={Home}/>
-                    <Route exact path="/cart" component={Cart}/>
-                    <Route exact path="/authenticate/local" component={Login}/>
-                    <Route exact path="/authenticate/register" component={Register}/>
-                    <Switch>
-                        <AdminRoute exact path="/admin" component={Landing}/>
-                        <AdminRoute exact path="/orders" component={OrderList}/>
-                        <AdminRoute exact path="/books" component={ProductList}/>
-                        <AdminRoute exact path="/bookType" component={BookTypeList}/>
-                        <AdminRoute exact path="/form/bookType" component={BookTypeForm}/>
-                        <AdminRoute exact path="/form/author" component={AuthorForm}/>
-                        <AdminRoute exact path="/form/publishinghouse" component={PublishingHouseForm}/>
-                        <AdminRoute exact path="/form/book" component={BookForm}/>
-                    </Switch>
-                </div>
-            </Router>
-        </Provider>
 
-    );
+if (localStorage.jwtToken) {
+    // Set auth token header auth
+    const token = localStorage.jwtToken;
+    setAuthToken(token);
+    // Decode token and get user info and exp
+    const decoded = jwt_decode(token);
+    // Set user and isAuthenticated
+    store.dispatch(setCurrentUser(decoded));
+    // Check for expired token
+    const currentTime = Date.now() / 1000; // to get in milliseconds
+    if (decoded.exp < currentTime) {
+        // Logout user
+        store.dispatch(logoutUser());
+
+        // Redirect to login
+        window.location.href = "./authenticate/login";
+    }
+}
+
+class App extends Component{
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <Navbar/>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/cart" component={Cart}/>
+                        <Route exact path="/authenticate/local" component={Login}/>
+                        <Route exact path="/authenticate/register" component={Register}/>
+                        <Switch>
+                            <AdminRoute exact path="/admin" component={Landing}/>
+                            <AdminRoute exact path="/orders" component={OrderList}/>
+                            <AdminRoute exact path="/books" component={ProductList}/>
+                            <AdminRoute exact path="/bookType" component={BookTypeList}/>
+                            <AdminRoute exact path="/form/bookType" component={BookTypeForm}/>
+                            <AdminRoute exact path="/form/author" component={AuthorForm}/>
+                            <AdminRoute exact path="/form/publishinghouse" component={PublishingHouseForm}/>
+                            <AdminRoute exact path="/form/book" component={BookForm}/>
+                        </Switch>
+                    </div>
+                </Router>
+            </Provider>
+
+        );
+    }
 }
 
 export default App;
